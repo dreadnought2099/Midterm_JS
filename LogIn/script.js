511b31b1c503fe7e5968e181223e4f2d7583fc9a
Raymart Magallanes <raymart.magallanes@bisu.edu.ph>
2024-10-18 00:38:34 +0800

    added new account

--- LogIn/script.js
+++ LogIn/script.js
@@ -9,16 +9,21 @@ function validateLogin(event) {
   // Hardcoded valid users
   const validUsers = {
     RMAGALLANEZ: "newt",
+    Yutang: "demon",
   };
 
   // Validate credentials
   if (validUsers[username] && validUsers[username] === password) {
     alert("Login successful!");
-    // Redirect to 'atm.html' on success
-    window.location.href = "../Profile/profile.html";
+
+    if (username === "RMAGALLANEZ") {
+      window.location.href = "../Profile/RMAGALLANEZ.html";
+    } else if (username === "Yutang") {
+      window.location.href = "../Profile/Yutang.html";
+    }
+
     return true;
   } else {
-    // Display error message on failure
     errorMessage.textContent = "Invalid username or password";
     return false;
   }
